0114fb59c6b895adaed9aa46d5d2d2b68544e750
Ravil Nuriev <rafnuriev@mail.ru>
2023-07-28 03:47:36 +0500

    Add HW_Django_templating

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Product
+
+
+def index(request):
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/index.html', context)
+
+def products(request):
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/products.html', context)
